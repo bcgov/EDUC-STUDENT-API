bf6b972a52ba03fc82eec6f0f5439600bb0cd32d
om <omprakashmishra3978@gmail.com>
2020-02-02 00:43:02 -0800

    updated unit tests to use h2 database than mocking for service.

--- api/src/test/java/ca/bc/gov/educ/api/student/service/StudentServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/student/service/StudentServiceTest.java
@@ -7,26 +7,25 @@ import ca.bc.gov.educ.api.student.repository.StudentRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class StudentServiceTest {
 
-  @InjectMocks
-  StudentService service;
-  @Mock
+  @Autowired
   StudentRepository repository;
+  StudentService service;
   private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
 
   @Before
@@ -36,46 +35,42 @@ public class StudentServiceTest {
 
   @Test
   public void testCreateStudent_WhenPayloadIsValid_ShouldReturnSavedObject() throws ParseException {
-
     StudentEntity student = getStudentEntity();
-    when(repository.save(student)).thenReturn(student);
     assertNotNull(service.createStudent(student));
+    assertNotNull(student.getStudentID());
   }
+
 
   @Test
   public void testCreateStudent_WhenPayloadContainsStudentID_ShouldThrowInvalidParameterException() throws ParseException {
     StudentEntity student = getStudentEntity();
     student.setStudentID(UUID.fromString("00000000-8000-0000-000e-000000000000"));
-    assertThrows(InvalidParameterException.class, () -> {
-      service.createStudent(student);
-    });
+    assertThrows(InvalidParameterException.class, () -> service.createStudent(student));
   }
 
 
   @Test
   public void testRetrieveStudent_WhenStudentExistInDB_ShouldReturnStudent() throws ParseException {
     StudentEntity student = getStudentEntity();
-    student.setStudentID(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000"));
-    when(repository.findById(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000"))).thenReturn(Optional.of(student));
+    assertNotNull(service.createStudent(student));
     assertNotNull(service.retrieveStudent(student.getStudentID()));
   }
 
   @Test
   public void testRetrieveStudent_WhenStudentDoesNotExistInDB_ShouldThrowEntityNotFoundException() {
-    when(repository.findById(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000"))).thenReturn(Optional.empty());
-    assertThrows(EntityNotFoundException.class, () -> service.retrieveStudent(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000")));
+    assertThrows(EntityNotFoundException.class, () -> service.retrieveStudent(UUID.fromString("00000000-0000-0000-0000-f3b2d4f20000")));
   }
 
   @Test
   public void testUpdateStudent_WhenPayloadIsValid_ShouldReturnTheUpdatedObject() throws ParseException {
 
     StudentEntity student = getStudentEntity();
-    student.setStudentID(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000"));
-    when(repository.findById(UUID.fromString("8e20a9c8-6ff3-12bf-816f-f3b2d4f20000"))).thenReturn(Optional.of(student));
-    when(repository.save(student)).thenReturn(student);
+    student = service.createStudent(student);
+    student.setLegalFirstName("updatedFirstName");
     StudentEntity updateEntity = service.updateStudent(student);
     assertNotNull(updateEntity);
     assertNotNull(updateEntity.getUpdateDate());
+    assertThat(updateEntity.getLegalFirstName().equals("updatedFirstName")).isTrue();
   }
 
 
