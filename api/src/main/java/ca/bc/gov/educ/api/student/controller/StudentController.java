9f763fed917d3bf8dbe168f84edb652e4ca299fc
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-02-20 13:26:08 -0800

    Change to add new PEN number.

--- api/src/main/java/ca/bc/gov/educ/api/student/controller/StudentController.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/controller/StudentController.java
@@ -1,27 +1,32 @@
 package ca.bc.gov.educ.api.student.controller;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RestController;
+
 import ca.bc.gov.educ.api.student.endpoint.StudentEndpoint;
 import ca.bc.gov.educ.api.student.exception.InvalidPayloadException;
 import ca.bc.gov.educ.api.student.exception.errors.ApiError;
 import ca.bc.gov.educ.api.student.mappers.StudentMapper;
+import ca.bc.gov.educ.api.student.model.StudentEntity;
 import ca.bc.gov.educ.api.student.properties.ApplicationProperties;
 import ca.bc.gov.educ.api.student.service.StudentService;
 import ca.bc.gov.educ.api.student.struct.Student;
 import ca.bc.gov.educ.api.student.validator.StudentPayloadValidator;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.UUID;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import lombok.extern.slf4j.Slf4j;
 
 
 /**
@@ -49,6 +54,11 @@ public class StudentController implements StudentEndpoint {
 
   public Student readStudent(String studentID) {
     return mapper.toStructure(getService().retrieveStudent(UUID.fromString(studentID)));
+  }
+  
+  public Iterable<Student> findStudent(String pen) {
+	  Optional<StudentEntity> studentsResponse = getService().retrieveStudentByPen(pen);
+	  return studentsResponse.map(Collections::singletonList).orElseGet(Collections::emptyList).stream().map(mapper::toStructure).collect(Collectors.toList());
   }
 
   public Student createStudent(Student student) {
@@ -93,4 +103,5 @@ public class StudentController implements StudentEndpoint {
     student.setCreateDate(new Date());
     student.setUpdateDate(new Date());
   }
+
 }
