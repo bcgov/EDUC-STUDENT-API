983e72a8e6ee370c491ca1f43c6dd39073cea545
mishraomp <omprakashmishra3978@gmail.com>
2020-06-21 22:02:23 -0700

    changes for unlink saga.

--- api/src/main/java/ca/bc/gov/educ/api/student/properties/ApplicationProperties.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/properties/ApplicationProperties.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.api.student.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
