a13c9009fd81db1e146c90b35ffae3fabce133a3
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-10-16 12:42:28 -0700

    Added deletes for student twins.

--- api/src/main/java/ca/bc/gov/educ/api/student/endpoint/StudentTwinEndpoint.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/endpoint/StudentTwinEndpoint.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.HttpStatus.CREATED;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("/")
 @OpenAPIDefinition(info = @Info(title = "API for Student Twin CRU.", description = "This CRU API is related to student twin data.", version = "1"), security = {@SecurityRequirement(name = "OAUTH2", scopes = {"READ_STUDENT", "WRITE_STUDENT", "READ_STUDENT_CODES"})})
@@ -32,4 +34,9 @@ public interface StudentTwinEndpoint {
   @GetMapping("student-twin-reason-codes")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   List<StudentTwinReasonCode> getStudentTwinReasonCodes();
+
+  @DeleteMapping("/{studentID}/twins/{studentTwinID}")
+  @PreAuthorize("#oauth2.hasScope('DELETE_STUDENT')")
+  @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "NO CONTENT"),  @ApiResponse(responseCode = "404", description = "NOT FOUND."), @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")})
+  ResponseEntity<Void> deleteById(@PathVariable UUID studentID, @PathVariable UUID studentTwinID);
 }
