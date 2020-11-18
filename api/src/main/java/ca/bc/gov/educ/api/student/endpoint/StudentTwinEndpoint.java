d945b6e53fc03489987e13c55c46591ae7bd5142
Mingwei <mingwei.y@gmail.com>
2020-11-18 09:30:10 -0800

    Fix code smells

--- api/src/main/java/ca/bc/gov/educ/api/student/endpoint/StudentTwinEndpoint.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/endpoint/StudentTwinEndpoint.java
@@ -1,11 +1,8 @@
 package ca.bc.gov.educ.api.student.endpoint;
 
 import ca.bc.gov.educ.api.student.struct.*;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
