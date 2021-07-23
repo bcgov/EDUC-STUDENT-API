ea70939922b406e2ea53a7007dcd3b57de2d8908
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-07-23 10:15:01 -0700

    code changes for logger and correlation-id

--- api/src/main/resources/db/migration/V1.0.26__STUDENT_API.sql
+++ api/src/main/resources/db/migration/V1.0.26__STUDENT_API.sql
@@ -1,5 +1,11 @@
---Modify Columns in STUDENT to make postal field 6
-ALTER TABLE STUDENT
+--Modify Columns in STUDENT to make postal field 7
+ALTER TABLE API_STUDENT.STUDENT
+    MODIFY (
+        POSTAL_CODE VARCHAR2(7)
+        );
+
+--Modify Columns in STUDENT to make postal field 7
+ALTER TABLE API_STUDENT.STUDENT_HISTORY
     MODIFY (
         POSTAL_CODE VARCHAR2(7)
         );
