269b820fca8b336624c1da759de2db40c5020b22
Trevor Richards <trev@trevdev.ca>
2024-12-11 10:22:29 -0800

    feat: convert dc to deployment
    * use selected branch for configmaps

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -6,6 +6,7 @@ DB_JDBC_CONNECT_STRING=$5
 DB_PWD=$6
 DB_USER=$7
 SPLUNK_TOKEN=$8
+BRANCH=$9
 
 TZVALUE="America/Vancouver"
 SOAM_KC_REALM_ID="master"
@@ -121,9 +122,9 @@ oc create -n "$OPENSHIFT_NAMESPACE-$envValue" configmap "$APP_NAME-config-map" \
   --dry-run -o yaml | oc apply -f -
 echo
 
-echo Setting environment variables for "$APP_NAME-$SOAM_KC_REALM_ID" application
+echo Setting environment variables for "$APP_NAME-$BRANCH" application
 oc -n "$OPENSHIFT_NAMESPACE-$envValue" set env \
-  --from="configmap/$APP_NAME-config-map" "dc/$APP_NAME-$SOAM_KC_REALM_ID"
+  --from="configmap/$APP_NAME-config-map" "deployment/$APP_NAME-$BRANCH"
 
 echo Creating config map "$APP_NAME-flb-sc-config-map"
 oc create -n "$OPENSHIFT_NAMESPACE-$envValue" configmap \
@@ -134,4 +135,4 @@ oc create -n "$OPENSHIFT_NAMESPACE-$envValue" configmap \
 
 echo Removing un-needed config entries
 oc -n "$OPENSHIFT_NAMESPACE"-"$envValue" set env \
-  dc/"$APP_NAME-$SOAM_KC_REALM_ID" KEYCLOAK_PUBLIC_KEY-
+  deployment/"$APP_NAME-$BRANCH" KEYCLOAK_PUBLIC_KEY-
